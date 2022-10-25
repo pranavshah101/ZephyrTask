afb599aef5b50629c0892ee2af05d82041921b70
Pranav Shah <32085761+pranavshah101@users.noreply.github.com>
2022-10-25 19:43:10 +0530

    Thread to thread communication working

--- src/main.c
+++ src/main.c
@@ -1,20 +1,36 @@
 #include <zephyr.h>
 #include <device.h>
 #include <sys/printk.h>
+#include <kernel.h>
+#include <string.h>
 
 
 #define STACKSIZE 1024   // stacksize
 #define PRIORITY 7        / Priority
 
 
-int delay = 1000;			
+
+
+int delay = 1000;
+
+struct printk_data_t {
+	void *fifo_reserved; /* 1st word reserved for use by fifo */
+	char tx_msg[50];
+    int time;
+
+};	
+
+K_FIFO_DEFINE(my_fifo);
+struct printk_data_t tx_message  = {.tx_msg = "Pranav SHah",.time = 1000};
 		
 void print1(void)
 {
 	while(1)
 	{
+   
 		printk("Hello World!");   // printing of the first thread
-		//printk( "%d\n", delay); // printing of delay
+      
+		printk( "%d\n", delay); // printing of delay
        
 		
 		
@@ -23,7 +39,9 @@ void print1(void)
 		{
 			delay=1000;
 		}
-		 k_msleep(delay);
+            k_fifo_put(&my_fifo,&tx_message);
+              k_msleep(delay);
+		 
 		
 		
 	}
@@ -31,12 +49,16 @@ void print1(void)
 
 void print2(void)
 {
+ 
+     
 	while(1)
 	{
-	     
-           k_msleep(1000);
-	     
-           printk("My name is Pranav Shah\n");  //Printing of second thread
+         
+	        struct printk_data_t *rx= k_fifo_get(&my_fifo, K_FOREVER);  
+            k_msleep(rx->time);
+	        printk("%s%d\n",rx->tx_msg,rx->time);
+      
+           //printk("My name is Pranav Shah\n");  //Printing of second thread
            
             
 		
@@ -44,5 +66,5 @@ void print2(void)
 	}
 }
 
-K_THREAD_DEFINE(print1_id, STACKSIZE,print1, NULL, NULL, NULL,PRIORITY, 0, 0); 
-K_THREAD_DEFINE(print2_id, STACKSIZE,print2, NULL, NULL, NULL,PRIORITY, 0, 0); 
+K_THREAD_DEFINE(print1_id, STACKSIZE,print1, NULL, NULL, NULL,7, 0, 0); 
+K_THREAD_DEFINE(print2_id, STACKSIZE,print2, NULL, NULL, NULL,7, 0, 0); 
